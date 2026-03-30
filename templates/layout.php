28f5c34e2931d12b50e44d8cc275f8dbe5d33b4a
kujirahand <web@kujirahand.com>
2026-03-30 12:09:18 +0900

    cssがキャッシュされないように

--- templates/layout.php
+++ templates/layout.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="public/style.css">
+    <link rel="stylesheet" href="public/style.css?v=<?= filemtime(__DIR__ . '/../public/style.css') ?>">
 </head>
 <body>
     <div class="app-container">
